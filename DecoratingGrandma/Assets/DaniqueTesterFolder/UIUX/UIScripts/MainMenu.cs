46184475fa717aa9635b0f171a99542985fdd098
sirfoxpoof <daniquedrost4@gmail.com>
2023-03-10 13:14:04 +0100

    Main menu test versie af
    Volume functionaliteit
    Quit functionaliteit
    New game functionaliteit

---
+++ DecoratingGrandma/Assets/DaniqueTesterFolder/UIUX/UIScripts/MainMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{ 
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quits game");
+        Application.Quit();
+    }
+
+ 
+
+
+}
+
---
+++ DecoratingGrandma/Assets/DaniqueTesterFolder/UIUX/UIScripts/VolumeSet.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Audio;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+public class VolumeSet : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public void SetVolumeLevel(float sliderValue)
+    {
+        mixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
+
+    }
+    
+
+    
+  
+}
