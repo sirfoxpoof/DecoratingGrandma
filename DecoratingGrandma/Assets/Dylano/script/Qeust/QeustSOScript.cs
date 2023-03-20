668f3a8a05276dbd754ab3e08c298857a570dc6a
Dylano Burgers <dylano.burgers@outlook.com>
2023-03-20 15:39:19 +0100

    test

---
+++ DecoratingGrandma/Assets/Dylano/script/Qeust/QeustSOScript.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.Scripting;
+
+[CreateAssetMenu(fileName = "New Text", menuName = "textSystem")]
+[System.Serializable]
+public class QeustSOScript : ScriptableObject
+{
+    public string[] dialouge;
+}
+ 
